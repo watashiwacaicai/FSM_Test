e2b3c4cf85bb3011112647a43d2aa6df88a31ff2
watashiwacaicai <88965149+watashiwacaicai@users.noreply.github.com>
2025-02-08 17:53:56 +0800

    增加pid精度

--- Middlewares/Lw_Pid/lw_pid.c
+++ Middlewares/Lw_Pid/lw_pid.c
@@ -10,13 +10,13 @@
 #define PID_MODE_FULLSCALE			4
 
 /*静态函数声明*/
-static float increment_pid_compute(Pid_object_t* pid_object, float current_input);
-static float fullscale_pid_compute(Pid_object_t* pid_object, float current_input);
+static double increment_pid_compute(Pid_object_t* pid_object, double current_input);
+static double fullscale_pid_compute(Pid_object_t* pid_object, double current_input);
 
 /*全局函数定义*/
 
 /*设置pid参数*/
-void pid_set_argument(Pid_object_t* pid_object, float kp, float ki, float kd)
+void pid_set_argument(Pid_object_t* pid_object, double kp, double ki, double kd)
 {
 	pid_object->kp = kp;
 	pid_object->ki = ki;
@@ -24,7 +24,7 @@ void pid_set_argument(Pid_object_t* pid_object, float kp, float ki, float kd)
 }
 
 /*设置pid目标值*/
-void pid_set_target(Pid_object_t* pid_object, float target)
+void pid_set_target(Pid_object_t* pid_object, double target)
 {
 	pid_object->target = target;
 }
@@ -42,7 +42,7 @@ void pid_output_limit_disable(Pid_object_t* pid_object)
 }
 
 /*设置pid输出限幅上下限*/
-void pid_set_output_limit(Pid_object_t* pid_object, float upper_limit, float floor_limit)
+void pid_set_output_limit(Pid_object_t* pid_object, double upper_limit, double floor_limit)
 {
 	pid_object->output_upper_limit = upper_limit;
 	pid_object->output_floor_limit = floor_limit;
@@ -61,7 +61,7 @@ void pid_intergral_limit_disable(Pid_object_t* pid_object)
 }
 
 /*设置pid的积分限幅上下限*/
-void pid_set_intergral_limit(Pid_object_t* pid_object, float upper_limit, float floor_limit)
+void pid_set_intergral_limit(Pid_object_t* pid_object, double upper_limit, double floor_limit)
 {
 	pid_object->intergral_upper_limit = upper_limit;
 	pid_object->intergral_floor_limit = floor_limit;
@@ -82,10 +82,10 @@ void pid_set_mode_fullscale(Pid_object_t* pid_object)
 /*初始化pid*/
 void pid_init(Pid_object_t* pid_object)
 {
-	pid_object->error0 = 0;
-	pid_object->error1 = 0;
-	pid_object->error2 = 0;
-	pid_object->error_intergral = 0;
+	pid_object->error0 = 0.0;
+	pid_object->error1 = 0.0;
+	pid_object->error2 = 0.0;
+	pid_object->error_intergral = 0.0;
 	
 	/*如果选择为增量式*/
 	if(pid_object->pid_mode == PID_MODE_INCREMENT)
@@ -101,9 +101,9 @@ void pid_init(Pid_object_t* pid_object)
 /*静态函数定义*/
 
 /*计算增量式pid*/
-static float increment_pid_compute(Pid_object_t* pid_object, float current_input)
+static double increment_pid_compute(Pid_object_t* pid_object, double current_input)
 {
-	float output;
+	double output;
 	
 	/*误差传递*/
 	pid_object->error2 = pid_object->error1;
@@ -130,9 +130,9 @@ static float increment_pid_compute(Pid_object_t* pid_object, float current_input
 }
 
 /*计算位置式pid*/
-static float fullscale_pid_compute(Pid_object_t* pid_object, float current_input)
+static double fullscale_pid_compute(Pid_object_t* pid_object, double current_input)
 {
-	float output;
+	double output;
 	
 	/*误差传递*/
 	pid_object->error1 = pid_object->error0;
--- Middlewares/Lw_Pid/lw_pid.h
+++ Middlewares/Lw_Pid/lw_pid.h
@@ -10,32 +10,32 @@ typedef struct Pid_object Pid_object_t;
 /*pid对象*/
 struct Pid_object
 {
-	float kp;
-	float ki;
-	float kd;
-	float target;
-	float error0; /*当前误差*/
-	float error1; /*上一次误差*/
-	float error2; /*上上次误差*/
-	float error_intergral; /*积分数值*/
-	float output_upper_limit; /*输出限幅上限*/
-	float output_floor_limit; /*输出限幅下限*/
-	float intergral_upper_limit; /*积分限幅上限*/
-	float intergral_floor_limit; /*积分限幅下限*/
+	double kp;
+	double ki;
+	double kd;
+	double target;
+	double error0; /*当前误差*/
+	double error1; /*上一次误差*/
+	double error2; /*上上次误差*/
+	double error_intergral; /*积分数值*/
+	double output_upper_limit; /*输出限幅上限*/
+	double output_floor_limit; /*输出限幅下限*/
+	double intergral_upper_limit; /*积分限幅上限*/
+	double intergral_floor_limit; /*积分限幅下限*/
 	uint8_t output_limit_state;	/*输出限幅的使用状态*/
 	uint8_t intergral_limit_state; /*积分限幅的使用状态*/
 	uint8_t pid_mode; /*pid的计算模式*/
-	float (*pid_compute)(Pid_object_t* pid_object, float current_input); /*pid计算句柄*/
+	double (*pid_compute)(Pid_object_t* pid_object, double current_input); /*pid计算句柄*/
 };
 
-void pid_set_argument(Pid_object_t* pid_object, float kp, float ki, float kd);
-void pid_set_target(Pid_object_t* pid_object, float target);
+void pid_set_argument(Pid_object_t* pid_object, double kp, double ki, double kd);
+void pid_set_target(Pid_object_t* pid_object, double target);
 void pid_output_limit_enable(Pid_object_t* pid_object);
 void pid_output_limit_disable(Pid_object_t* pid_object);
-void pid_set_output_limit(Pid_object_t* pid_object, float upper_limit, float floor_limit);
+void pid_set_output_limit(Pid_object_t* pid_object, double upper_limit, double floor_limit);
 void pid_intergral_limit_enable(Pid_object_t* pid_object);
 void pid_intergral_limit_disable(Pid_object_t* pid_object);
-void pid_set_intergral_limit(Pid_object_t* pid_object, float upper_limit, float floor_limit);
+void pid_set_intergral_limit(Pid_object_t* pid_object, double upper_limit, double floor_limit);
 void pid_set_mode_increment(Pid_object_t* pid_object);
 void pid_set_mode_fullscale(Pid_object_t* pid_object);
 void pid_init(Pid_object_t* pid_object);
